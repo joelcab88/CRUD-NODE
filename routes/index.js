c65b175cfc4dc6821ca16724860078f739f63769
Joel Cab Yah <jocab@comasw.com>
2022-08-23 13:33:52 -0500

    JFCABY :,Se remueve ruta de libros ya que se migró a un nuevo archivo de rutas.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const libroController =  require('../controllers/librosController');
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express-Node-test" });
+  res.send("Bienvenido.")
 });
-
-router.get("/libros", libroController.index);
 
 module.exports = router;
